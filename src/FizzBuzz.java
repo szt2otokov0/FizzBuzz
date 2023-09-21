a53b97b59cee59176a7a71166d20e98d58e555e4
szt2otokov <szt2otokov@vasvari.org>
2023-09-21 12:53:36 +0200

    dun

---
+++ src/FizzBuzz.java
@@ -0,0 +1,10 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for(int i = 0; i< 100;i++){
+            if(i % 3 == 0) System.out.print("Fizz");
+            if(i % 5 == 0) System.out.print("Buzz");
+            else if(i % 3 != 0) System.out.print(i);
+            System.out.print('\n');
+        }
+    }
+}
